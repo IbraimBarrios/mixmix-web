2caac4880c02e8596cf5b7403d1fe56ce74522cd
Ibraim Barrios <barrios.party@hotmail.com>
2025-09-10 18:32:41 -0600

    Remove function in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,3 @@
 export const lowerCaseText = (text: string) => {
   return text.toLowerCase();
 };
-
-export const underscoresToSpaces = (text: string) => {
-  return text.replaceAll("_", " ");
-};
-
-export const spacesToUnderscores = (text: string) => {
-  return text.replaceAll(" ", "_");
-};
